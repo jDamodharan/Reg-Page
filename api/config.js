363d4d39ead5c7e2b9e8a3f55cbe366205c55333
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-11-27 20:38:14 +0530

    Pages Added

---
+++ api/config.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        serviceId: process.env.SERVICE_ID,
+        templateOwner: process.env.TEMPLATE_OWNER,
+        templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
+        publicKey: process.env.PUBLIC_KEY
+    });
+}
