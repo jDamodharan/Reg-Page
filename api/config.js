48ad8938d2336b2b001c31264010bb5ab8a8fc00
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-11-29 22:25:22 +0530

    fix1

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
-    res.status(200).json({
-        serviceId: process.env.SERVICE_ID,
-        templateOwner: process.env.TEMPLATE_OWNER,
-        templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
-        publicKey: process.env.PUBLIC_KEY
-    });
+  res.status(200).json({
+    serviceId: process.env.SERVICE_ID,
+    templateOwner: process.env.TEMPLATE_OWNER,
+    templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
+    publicKey: process.env.PUBLIC_KEY
+  });
 }
